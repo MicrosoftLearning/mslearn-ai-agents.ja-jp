bbae5475ef6b6320c23ccb69a44f74822f594195
olprod <olprod@microsoft.com>
2026-02-24 09:25:25 +0800

    Copy Files From Source Repo (2026-02-24 09:25)

--- Labfiles/07-agent-framework/python/agent-framework.py
+++ Labfiles/07-agent-framework/python/agent-framework.py
@@ -24,10 +24,13 @@ async def main():
     
 async def process_expenses_data(prompt, expenses_data):
 
-    # Create a client and initialize an agent with the tool and instructions
+    # Create an Azure OpenAI client
+
+
+    # Create an agent with the tool and instructions
     
 
-        # Use the agent to process the expenses data    
+    # Use the agent to process the expenses data    
 
 
 
